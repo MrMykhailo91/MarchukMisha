a4cff143520afd6d679d970a8eb934e8b0aa64ad
MishaMarchuk <marchukmisha91@gmail.com>
2026-06-22 18:19:44 +0300

    lesson3 2

---
+++ lesson3/script.js
@@ -0,0 +1,4 @@
+const numberOne = prompt("Введіть перше число.");
+const numberTwo = prompt("Введіть друге число.");
+const numberTree = prompt("Введіть третє число.");
+console.log(numberOne, numberTwo, numberTree);
