ff48c4356f64eb3844474ee59a1f13cbdbec0c4d
MishaMarchuk <marchukmisha91@gmail.com>
2026-07-05 13:56:36 +0300

    lesson7(2)

---
+++ lesson7/task2/script.js
@@ -0,0 +1,11 @@
+const data = [-3, 2, undefined, 0, 22, true, null, 55];
+function numerical(acc, items) {
+	const result = data.filter((arr) => {
+		if (typeof arr === "number") return true;
+	});
+	const sum = result.reduce((arr, item) => {
+		arr += item;
+		return arr;
+	}, 0);
+}
+numerical();
